2e98e153bc42a2c57d4dad75d9ac677d52a1919e
joaoVitorLeal <joaoleal98@outlook.com>
2025-07-23 13:58:05 -0300

    refactor: delete deprecated Java classes

--- src/main/java/io/github/joaoVitorLeal/gof_with_spring/services/ViaCepService.java
+++
@@ -1,8 +0,0 @@
-package io.github.joaoVitorLeal.gof_with_spring.services;
-
-import io.github.joaoVitorLeal.gof_with_spring.models.Address;
-
-public interface ViaCepService {
-
-    Address findAddress(String cep);
-}
